5d929d3aabe77b50bb42499c40497f18521da4a7
Lucas Farias <lukf95@gmail.com>
2019-09-15 01:52:26 -0300

    Fix

--- src/db/RENAME_config.js
+++
@@ -1,4 +0,0 @@
-// Rename to config.js
-const url = 'postgres://';
-
-module.exports = url;
---
+++ src/db/config.js
@@ -0,0 +1,5 @@
+// This file must not be commited
+const url =
+  'postgres://rlfbjaflsaqnrq:01602e3c0f98099f523bf3f1e0dcd91538e8b14232f8b568cf86810ce6dcd231@ec2-174-129-27-158.compute-1.amazonaws.com:5432/dehaepah7qr6fi';
+
+module.exports = url;
